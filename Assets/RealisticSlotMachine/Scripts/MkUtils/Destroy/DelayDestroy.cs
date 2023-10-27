f7186778342a0a8c115c87bae5b89167b225cbd8
ChoiJungHwan <cjh6220@gmail.com>
2023-10-27 14:02:10 +0900

    슬릇머신 에셋 추가

---
+++ Assets/RealisticSlotMachine/Scripts/MkUtils/Destroy/DelayDestroy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+/*
+    
+    25.10.2021 - destroy any gameobject
+    27.08.2020 - first
+ */
+namespace Mkey
+{
+    public class DelayDestroy : MonoBehaviour
+    {
+        public float time = 0.0f;
+        [Tooltip("Destroy auto this gameobject after Awake.")]
+        public bool auto = true;
+
+        void Awake()
+        {
+            if(auto)  DestroyGameObject();
+        }
+
+        public void DestroyGameObject()
+        {
+            Destroy(gameObject, time);
+        }
+
+        public void DestroyGameObject(GameObject gO)
+        {
+            if (gO) Destroy(gO, time);
+        }
+    }
+}
